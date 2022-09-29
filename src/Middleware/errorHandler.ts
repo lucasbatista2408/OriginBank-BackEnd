c299e650b2f9ecc1a41f56f02c485d04c2f319c5
lucas <lucasbatista2408@gmail.com>
2022-09-29 17:48:43 -0300

    adds error handler file and @types to jwt

---
+++ src/Middleware/errorHandler.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from "express";
+
+
+export default function errorHandlingMiddleware (error: any, req: Request, res: Response, next: NextFunction) {
+	
+	if (error.type === "error_not_found") {
+		return res.status(404).send(error.message);
+	}
+	if (error.type === "unprocessable") {
+		return res.status(422).send(error.message);
+	}
+	if (error.type === "conflict") {
+		return res.status(409).send(error.message);
+	}
+	if(error.type === "forbidden"){
+		return res.status(403).send(error.message);
+	}
+	if(error.type === "could_not_update" || error.type === "bad_request"){
+		return res.status(400).send(error.message);
+	}
+
+	res.sendStatus(500); 
+}
